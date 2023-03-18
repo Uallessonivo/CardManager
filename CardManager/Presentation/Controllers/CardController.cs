1616f419b19572da8146f9fa47b22d201f3dca75
Uallesson Nunes Ivo <uallessons@gmail.com>
2023-03-18 13:44:19 -0300

    feat(Presentation): added new functions to the controller

--- CardManager/Presentation/Controllers/CardController.cs
+++ CardManager/Presentation/Controllers/CardController.cs
@@ -1,4 +1,5 @@
 ﻿using CardManager.Application.Interfaces;
+using CardManager.Domain.Enums;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CardManager.Presentation.Controllers
@@ -12,6 +13,34 @@ namespace CardManager.Presentation.Controllers
         public CardController(ICardService cardService)
         {
             _cardService = cardService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCards()
+        {
+            var cards = await _cardService.GetAllAsync();
+            return Ok(cards);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdAsync(Guid id)
+        {
+            try
+            {
+                var card = await _cardService.GetByIdAsync(id);
+                return Ok(card);
+            } 
+            catch (Exception ex)
+            {
+                return StatusCode(404, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCardByType([FromRoute] CardType type)
+        {
+            var cards = await _cardService.GetAllByType(type);
+            return Ok(cards);
         }
     }
 }
