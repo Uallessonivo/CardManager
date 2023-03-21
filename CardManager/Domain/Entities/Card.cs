749150f2d6f84378c9082e75339aef160754f975
Uallessonivo <uallessons@gmail.com>
2023-03-21 17:24:54 -0300

    refactor(Domain): added the`nullable` checks to attributes of my `Card` class

--- CardManager/Domain/Entities/Card.cs
+++ CardManager/Domain/Entities/Card.cs
@@ -5,9 +5,9 @@ namespace CardManager.Domain.Entities
     public class Card
     {
         public Guid CardId { get; set; }
-        public string CardSerial { get; set; }
-        public string CardOwnerName { get; set; }
-        public string CardOwnerCpf { get; set; }
+        public string? CardSerial { get; set; }
+        public string? CardOwnerName { get; set; }
+        public string? CardOwnerCpf { get; set; }
         public CardType CardType { get; set; }
     }
 }
