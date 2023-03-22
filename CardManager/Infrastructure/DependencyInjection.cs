e1e3619a7b3b29791c96d137731d9e1b80436873
Uallessonivo <uallessons@gmail.com>
2023-03-22 15:55:27 -0300

    refactor(Infra): impl dependency injection to the repositories

---
+++ CardManager/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using CardManager.Infrastructure.Interfaces;
+using CardManager.Infrastructure.Persistence;
+using CardManager.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace CardManager.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<CardManagerDbContextInMemory>(options =>
+                options.UseInMemoryDatabase("CardManagerDb"));
+            services.AddScoped<ICardRepository, CardRepository>();
+            return services;
+        }
+    }
+}
