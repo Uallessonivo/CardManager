cb2407f370773076268aa623f02fcb7442c91640
Uallessonivo <uallessons@gmail.com>
2023-03-23 14:44:33 -0300

    feat(Project): turning on the swagger api

--- CardManager/Program.cs
+++ CardManager/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // Service layer
 builder.Services.AddApplication();
@@ -13,6 +15,12 @@ builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
