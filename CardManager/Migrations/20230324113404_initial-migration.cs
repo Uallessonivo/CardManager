5d4f6b8f994b4f29c2fda7df0cbaa10e518af2f4
Uallessonivo <uallessons@gmail.com>
2023-03-24 16:55:19 -0300

    update project to use postgres database and created the migrations

---
+++ CardManager/Migrations/20230324113404_initial-migration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CardManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cards",
+                columns: table => new
+                {
+                    CardId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CardSerial = table.Column<string>(type: "text", nullable: true),
+                    CardOwnerName = table.Column<string>(type: "text", nullable: true),
+                    CardOwnerCpf = table.Column<string>(type: "text", nullable: true),
+                    CardType = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cards", x => x.CardId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cards");
+        }
+    }
+}
