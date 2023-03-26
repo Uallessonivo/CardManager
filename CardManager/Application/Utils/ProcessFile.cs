58d77af2e092016a15dde32b714632b647866710
Uallesson Nunes Ivo <uallessons@gmail.com>
2023-03-26 07:52:12 -0300

    feat(application): created Parse method for handling uploaded files

---
+++ CardManager/Application/Utils/ProcessFile.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using CardManager.Presentation.DTO;
+using CsvHelper;
+
+namespace CardManager.Application.Utils
+{
+    public static class ProcessFile
+    {
+        public static async Task<List<CardDto>> Parse(IFormFile file)
+        {
+            var reader = new StreamReader(file.OpenReadStream());
+            var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture);
+            var cards = csvReader.GetRecords<CardDto>().ToList();
+            return cards;
+        }
+    }
+}
