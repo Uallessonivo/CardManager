95aafe1c82d40a38a88dedb5c10df7355da849d2
Uallessonivo <uallessons@gmail.com>
2023-03-29 09:44:01 -0300

    feat(Application): new validator for UpdateCardDto.cs

--- CardManager/Application/Validators/UpdateCardValidator.cs
+++ CardManager/Application/Validators/UpdateCardValidator.cs
@@ -4,20 +4,15 @@ using FluentValidation;
 
 namespace CardManager.Application.Validators
 {
-    public class CardValidator : AbstractValidator<CardDto>
+    public class UpdateCardValidator : AbstractValidator<UpdateCardDto>
     {
-        public CardValidator()
+        public UpdateCardValidator()
         {
             RuleFor(c => c.CardOwnerCpf)
                 .NotEmpty().WithMessage("O CPF não pode estar vazio.")
                 .Length(11).WithMessage("O CPF deve conter 11 dígitos.");
-            RuleFor(c => c.CardSerial)
-                .NotEmpty().WithMessage("O Serial não pode estar vazio.")
-                .Length(15).WithMessage("O Serial deve conter 15 dígitos.");
             RuleFor(c => c.CardOwnerName)
                 .NotEmpty().WithMessage("O nome não pode estar vazio.");
-            RuleFor(c => c.CardType)
-                .NotEmpty().WithMessage("O tipo não pode estar vazio.");
         }
     }
 }
