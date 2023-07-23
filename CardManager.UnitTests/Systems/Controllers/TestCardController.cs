064e9424a3e6c0ee870c61fd54b2596425b5f8c1
Uallesson Nunes Ivo <uallessons@gmail.com>
2023-07-23 15:48:29 -0300

    New module for testings

---
+++ CardManager.UnitTests/Systems/Controllers/TestCardController.cs
@@ -0,0 +1,13 @@
+namespace CardManager.UnitTests.Systems.Controllers
+{
+    public class TestCardController
+    {
+        [Fact]
+        public void Should_Return_All_Cards()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+    }
+}
