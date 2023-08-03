da6ec9577606c94c6782c6139b957b8ab0e27598
Uallessonivo <uallessons@gmail.com>
2023-08-03 17:20:22 -0300

    REFACTOR: standardizing the endpoints return

--- CardManager/Presentation/Controllers/CardController.cs
+++ CardManager/Presentation/Controllers/CardController.cs
@@ -11,177 +11,210 @@ namespace CardManager.Presentation.Controllers
     public class CardController : ControllerBase
     {
         private readonly ICardService _cardService;
+        private readonly ResponseDto _responseDto;
 
         public CardController(ICardService cardService)
         {
             _cardService = cardService;
+            _responseDto = new ResponseDto();
         }
 
         [HttpGet("list-cards")]
-        public async Task<IActionResult> GetCards()
+        public async Task<ResponseDto> GetCards()
         {
             try
             {
                 var cards = await _cardService.GetAllAsync();
-                return Ok(cards);
+                _responseDto.Result = cards;
             }
             catch (Exception ex)
             {
-                return StatusCode(NotFound().StatusCode, ex.Message);
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
             }
+
+            return _responseDto;
         }
 
         [HttpGet("list-cards-paginated")]
-        public async Task<IActionResult> GetCards([FromQuery] int pageNumber, [FromQuery] int pageSize)
+        public async Task<ResponseDto> GetCards([FromQuery] int pageNumber, [FromQuery] int pageSize)
         {
             try
             {
                 var cards = await _cardService.GetAllPaginatedAsync(pageNumber, pageSize);
-                return Ok(cards);
+                _responseDto.Result = cards;
             }
             catch (Exception ex)
             {
-                return StatusCode(NotFound().StatusCode, ex.Message);
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
             }
+
+            return _responseDto;
         }
 
         [HttpGet("filter-card-id")]
-        public async Task<IActionResult> GetByIdAsync([FromQuery] Guid id)
+        public async Task<ResponseDto> GetByIdAsync([FromQuery] Guid id)
         {
             try
             {
                 var card = await _cardService.GetByIdAsync(id);
-                return Ok(card);
+                _responseDto.Result = card;
             }
             catch (Exception ex)
             {
-                return StatusCode(NotFound().StatusCode, ex.Message);
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
             }
+
+            return _responseDto;
         }
 
         [HttpGet("filter-card-type")]
-        public async Task<IActionResult> GetCardByType([FromQuery] CardType type)
+        public async Task<ResponseDto> GetCardByType([FromQuery] CardType type)
         {
             try
             {
                 var cards = await _cardService.GetAllByType(type);
-                return Ok(cards);
+                _responseDto.Result = cards;
             }
             catch (Exception ex)
             {
-                return StatusCode(NotFound().StatusCode, ex.Message);
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
             }
+
+            return _responseDto;
         }
 
         [HttpGet("filter-card-cpf")]
-        public async Task<IActionResult> GetCardByOwnerCpf([FromQuery] string owner)
+        public async Task<ResponseDto> GetCardByOwnerCpf([FromQuery] string owner)
         {
             try
             {
                 var cards = await _cardService.GetByOwnerCpfAsync(owner);
-                return Ok(cards);
+                _responseDto.Result = cards;
             }
             catch (Exception ex)
             {
-                return StatusCode(NotFound().StatusCode, ex.Message);
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
             }
+
+            return _responseDto;
         }
 
         [HttpGet("generate-report")]
-        public async Task<FileResult> DownloadReport([FromQuery] string type)
+        public async Task<ResponseDto> DownloadReport([FromQuery] string type)
         {
-
             var csvData = await _cardService.GenerateReport(type);
             var bytes = Encoding.UTF8.GetBytes(csvData.ToString());
             var stream = new MemoryStream(bytes);
-            var contentType = "text/csv";
-            var fileName = $"{DateTime.Now:yyyyMMdd}-{type}.csv";
 
-            return File(stream, contentType, fileName);
+            _responseDto.Result = stream;
+            return _responseDto;
         }
 
         [HttpPost("create-card")]
-        public async Task<IActionResult> CreateCard([FromBody] CardDto cardDto)
+        public async Task<ResponseDto> CreateCard([FromBody] CardDto cardDto)
         {
             try
             {
                 await _cardService.CreateCardAsync(cardDto);
-                return Ok();
+                _responseDto.Message = "Cartão criado com sucesso.";
             }
             catch (Exception ex)
             {
-                return StatusCode(BadRequest().StatusCode, ex.Message);
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
             }
+
+            return _responseDto;
         }
 
         [HttpPost("seed-database")]
-        public async Task<IActionResult> UploadFileAndSeedDatabase(IFormFile file)
+        public async Task<ResponseDto> UploadFileAndSeedDatabase(IFormFile file)
         {
             try
             {
                 var response = await _cardService.SeedDatabaseTask(file);
-                return Ok(response);
+                _responseDto.Result = response;
             }
             catch (Exception ex)
             {
-                return StatusCode(BadRequest().StatusCode, ex.Message);
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
             }
+
+            return _responseDto;
         }
 
         [HttpPost("update-card")]
-        public async Task<IActionResult> UpdateCard([FromQuery] Guid id, [FromBody] UpdateCardDto updateCardDto)
+        public async Task<ResponseDto> UpdateCard([FromQuery] Guid id, [FromBody] UpdateCardDto updateCardDto)
         {
             try
             {
                 await _cardService.UpdateCardAsync(id, updateCardDto);
-                return Ok();
+                _responseDto.Message = "Cartão atualizado com sucesso.";
             }
             catch (Exception ex)
             {
-                return StatusCode(BadRequest().StatusCode, ex.Message);
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
             }
+            
+            return _responseDto;
         }
 
         [HttpDelete("delete-card")]
-        public async Task<IActionResult> DeleteCard([FromQuery] Guid id)
+        public async Task<ResponseDto> DeleteCard([FromQuery] Guid id)
         {
             try
             {
                 await _cardService.DeleteCardAsync(id);
-                return Ok();
+                _responseDto.Message = "Cartão apagado com sucesso.";
             }
             catch (Exception ex)
             {
-                return StatusCode(BadRequest().StatusCode, ex.Message);
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
             }
+            
+            return _responseDto;
         }
-        
+
         [HttpDelete("delete-card-by")]
-        public async Task<IActionResult> DeleteCard([FromQuery] string cardSerial)
+        public async Task<ResponseDto> DeleteCard([FromQuery] string cardSerial)
         {
             try
             {
                 await _cardService.DeleteCardAsync(cardSerial);
-                return Ok();
+                _responseDto.Message = "Cartão apagado com sucesso.";
             }
             catch (Exception ex)
             {
-                return StatusCode(BadRequest().StatusCode, ex.Message);
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
             }
+            
+            return _responseDto;
         }
 
         [HttpDelete("delete-all-cards")]
-        public async Task<IActionResult> DeleteAllCards()
+        public async Task<ResponseDto> DeleteAllCards()
         {
             try
             {
                 await _cardService.DeleteAllCardsAsync();
-                return Ok("Todos os cartões foram apagados da base.");
+                _responseDto.Message = "Todos os cartões foram apagados com sucesso.";
             }
             catch (Exception ex)
             {
-                return StatusCode(BadRequest().StatusCode, ex.Message);
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
             }
+            
+            return _responseDto;
         }
     }
 }
