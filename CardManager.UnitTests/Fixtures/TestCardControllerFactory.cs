e5f8b41a6805b7724cf39c42efdd36ac30ea39a7
Uallessonivo <uallessons@gmail.com>
2024-12-27 19:13:45 -0300

    feat: add TestCardControllerFactory.cs to process controllers tests

---
+++ CardManager.UnitTests/Fixtures/TestCardControllerFactory.cs
@@ -0,0 +1,20 @@
+﻿using CardManager.Application.DTO;
+using CardManager.Application.Interfaces;
+using CardManager.Presentation.Controllers;
+using Moq;
+
+namespace CardManager.UnitTests.Fixtures;
+
+public class CardControllerFixture
+{
+    public Mock<ICardService> CardServiceMock { get; private set; }
+    public Mock<ResponseDto> ResponseDtoMock { get; private set; }
+    public CardController CardController { get; private set; }
+
+    public CardControllerFixture()
+    {
+        CardServiceMock = new Mock<ICardService>();
+        ResponseDtoMock = new Mock<ResponseDto>();
+        CardController = new CardController(CardServiceMock.Object);
+    }
+}
