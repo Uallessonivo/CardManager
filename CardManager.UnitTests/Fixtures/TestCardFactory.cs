c653d9f42b5704b6482549aef532584d545e97d7
Uallessonivo <uallessons@gmail.com>
2024-12-28 06:40:18 -0300

    feat: add new fake cardDto list to factory test

--- CardManager.UnitTests/Fixtures/TestCardFactory.cs
+++ CardManager.UnitTests/Fixtures/TestCardFactory.cs
@@ -1,4 +1,5 @@
-﻿using CardManager.Domain.Entities;
+﻿using CardManager.Application.DTO;
+using CardManager.Domain.Entities;
 using CardManager.Domain.Enums;
 
 namespace CardManager.UnitTests.Fixtures
@@ -36,6 +37,37 @@ namespace CardManager.UnitTests.Fixtures
                     CardOwnerName = "THIS IS THE OTHER NAME",
                     CardSerial = "000000000000000",
                     CardType = CardType.Incentivo
+                }
+            };
+        }
+        
+        public static IEnumerable<CardDto> FakeCardsDto()
+        {
+            return new List<CardDto>
+            {
+                new CardDto {
+                    CardOwnerCpf = "000000000000",
+                    CardOwnerName = "THIS IS THE NAME",
+                    CardSerial = "000000000000000",
+                    CardType = "Despesas"
+                },
+                new CardDto {
+                    CardOwnerCpf = "11111111111",
+                    CardOwnerName = "THIS IS THE OTHER NAME",
+                    CardSerial = "000000000000000",
+                    CardType = "Despesas"
+                },
+                new CardDto {
+                    CardOwnerCpf = "22222222222",
+                    CardOwnerName = "THIS IS THE OTHER NAME",
+                    CardSerial = "000000000000000",
+                    CardType = "Incentivo"
+                },
+                new CardDto {
+                    CardOwnerCpf = "33333333333",
+                    CardOwnerName = "THIS IS THE OTHER NAME",
+                    CardSerial = "000000000000000",
+                    CardType = "Incentivo"
                 }
             };
         }
